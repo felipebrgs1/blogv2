970a9e6c8a72d668f03066fc4120cb7a711c5f80
felipeborgaco <felipeborgaco@hotmail.com>
2026-03-05 15:23:38 -0300

    ,

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: cloudflare({
-		imageService: "compile",
-	}),
+	output: "static",
 	integrations: [mdx()],
 	vite: {
 		plugins: [tailwindcss()],
