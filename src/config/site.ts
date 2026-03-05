635714c936e2c816c9a92177885e15af028d06e6
felipeborgaco <felipeborgaco@hotmail.com>
2026-03-05 15:10:35 -0300

    .

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
 	name: "The Lab",
-	url: "https://blogv2.felipeborgacogame.workers.dev",
+	url: "https://blog.felipebrg.com.br",
 	social: {
 		github: "https://github.com/felipebrgs1",
 		linkedin: "https://www.linkedin.com/in/felipeborgaco/",
